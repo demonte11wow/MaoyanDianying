4127bd23005e2589c60bf32ec6979b8f33c9edc4
chen <273121612@qq.com>
2016-12-03 18:44:36 +0800

    城市页面 step2 快速索引

--- app/src/main/java/com/example/johnsnow/maoyandianying/city/fragment/CityFragment.java
+++ app/src/main/java/com/example/johnsnow/maoyandianying/city/fragment/CityFragment.java
@@ -1,19 +1,23 @@
 package com.example.johnsnow.maoyandianying.city.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.johnsnow.maoyandianying.Base.DividerDecoration;
 import com.example.johnsnow.maoyandianying.R;
 import com.example.johnsnow.maoyandianying.city.adapter.CityFindAdapter;
 import com.example.johnsnow.maoyandianying.city.bean.CityBean;
+import com.example.johnsnow.maoyandianying.city.view.IndexView;
 import com.example.johnsnow.maoyandianying.utils.TReadUtils;
 import com.google.gson.Gson;
 import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersDecoration;
@@ -29,6 +33,11 @@ public class CityFragment extends Fragment {
     TReadUtils rt;
     String c_list;
     List<String> pinyingList;
+    private TextView tv_main_word;
+
+    private IndexView iv_main_words;//索引
+
+    private Handler handler = new Handler();
 
     public static Fragment newInstance() {
         return new CityFragment();
@@ -40,15 +49,87 @@ public class CityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_city, container, false);
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
         initReList();
+        iv_main_words = (IndexView) view.findViewById(R.id.iv_main_words);
+        tv_main_word = (TextView) view.findViewById(R.id.tv_main_word);
+
+//        iv_main_words.setOnIndexChangeListener(new IndexView.OnIndexChangeListener() {
+//            @Override
+//            public void onIndexChange(String word) {
+//                updateWord(word);
+//                updateListView(word);
+//            }
+//        });
+
+        iv_main_words
+                .setOnTouchLetterChangeListenner(new IndexView.OnTouchLetterChangeListenner() {
+
+                    @Override
+                    public void onTouchLetterChange(MotionEvent event, String s) {
+
+                        tv_main_word.setText(s);
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN:
+                            case MotionEvent.ACTION_MOVE:
+                                tv_main_word.setVisibility(View.VISIBLE);
+                                break;
+
+                            case MotionEvent.ACTION_UP:
+                            default:
+                                tv_main_word.postDelayed(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                                        tv_main_word.setVisibility(View.GONE);
+                                    }
+                                }, 100);
+                                break;
+                        }
+//                        int position = pinyingList.;//这个array就是传给自定义Adapter的
+//                        recyclerView.scrollToPosition(position);
+
+                        for (int i = 0; i < pinyingList.size(); i++) {
+//                            String preWord = pinyingList.get(i).substring(0, 1);
+                            if (pinyingList.get(i).startsWith(s)) {
+                                recyclerView.scrollToPosition(i);
+                                break;
+                            }
+                        }
+                    }
+                });
+
+
         return view;
     }
 
+//    private void updateListView(String word) {
+//
+//        for(int i = 0; i < pinyingList.size(); i++) {
+//            String preWord= pinyingList.get(i).substring(0,1);
+//            if(preWord.equals(word)) {
+//                recyclerView.scrollToPosition(i);
+//            }
+//        }
+//    }
+//
+//    private void updateWord(String word) {
+//        tv_main_word.setText(word);
+//        tv_main_word.setVisibility(View.VISIBLE);
+//        handler.removeCallbacksAndMessages(0);
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                tv_main_word.setVisibility(View.GONE);
+//            }
+//        },2000);
+//    }
+
+
     private void initReList() {
-        Log.e("chentTAG","i BEGIN");
+        Log.e("chentTAG", "i BEGIN");
         rt = new TReadUtils(getActivity());
         c_list = rt.readJson();
 
-        Log.e("chentTAG",c_list);
+        Log.e("chentTAG", c_list);
 
         Gson gson = new Gson();
         CityBean cb = gson.fromJson(c_list, CityBean.class);//CityName.cityNameList
@@ -78,17 +159,13 @@ public class CityFragment extends Fragment {
         recyclerView.addItemDecoration(new DividerDecoration(getActivity()));
     }
 
-    private void bubble_sort(List<CityBean.DataBean> cbNameList)
-    {
-        for (int i = 0; i < cbNameList.size(); i++)
-        {
-            for (int j = i; j < cbNameList.size(); j++)
-            {
-                if (cbNameList.get(i).getPinyin().charAt(0) > cbNameList.get(j).getPinyin().charAt(0))
-                {
+    private void bubble_sort(List<CityBean.DataBean> cbNameList) {
+        for (int i = 0; i < cbNameList.size(); i++) {
+            for (int j = i; j < cbNameList.size(); j++) {
+                if (cbNameList.get(i).getPinyin().charAt(0) > cbNameList.get(j).getPinyin().charAt(0)) {
                     CityBean.DataBean temp = cbNameList.get(i);
-                    cbNameList.set(i,cbNameList.get(j));
-                    cbNameList.set(j,temp);
+                    cbNameList.set(i, cbNameList.get(j));
+                    cbNameList.set(j, temp);
                 }
             }
         }
